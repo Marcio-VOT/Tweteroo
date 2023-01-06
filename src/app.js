661d149732ea09d8b94149b4b0559ed15846fa1d
Marcio Fran <115326340+Marcio-VOT@users.noreply.github.com>
2023-01-06 23:48:04 +0000

    corpo requisições

--- src/app.js
+++ src/app.js
@@ -1,1 +1,31 @@
+import express from 'express'
 
+const app = express();
+app.use(express.json());
+
+const user = [{
+	username: 'bobesponja', 
+	avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info" 
+}];
+const tweet = [{
+	username: "bobesponja",
+  tweet: "eu amo o hub"
+}]
+
+app.post('/sign-up', (req, res) => {
+	const newUser = req.body
+  user.push(newUser);
+  res.send("OK");
+});
+app.post('/tweets', (req, res) => {
+	const pessoa = req.body
+    
+  pessoas.push(pessoa);
+  res.send(pessoa);
+});
+
+app.get('/tweets', (req, res) => {
+  res.send(pessoas);
+});
+
+app.listen(5000);
