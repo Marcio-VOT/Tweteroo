16e646b55fc77589aacc08958594469e42cc3f83
Marcio Fran <115326340+Marcio-VOT@users.noreply.github.com>
2023-01-09 03:47:03 +0000

    removendo conteúdo de teste

--- src/app.js
+++ src/app.js
@@ -7,14 +7,8 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const userList = [{
-	username: 'bobesponja', 
-	avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info" 
-}];
-let tweet = [{
-	username: "bobesponja",
-  tweet: "eu amo o hub"
-}];
+const userList = [];
+let tweet = [];
 
 app.post('/sign-up', (req, res) => {
 	const newUser = req.body;
